932591f4d7a43d81e6640c467405278edde2ca91
terrayuku <peshyuku@gmail.com>
2017-05-10 16:21:04 +0200

    Student Subject returning class

--- src/main/java/com/db/DBAccess.java
+++ src/main/java/com/db/DBAccess.java
@@ -399,7 +399,8 @@ public List getStudent_Subject(String exam_num) throws URISyntaxException
         call.setString(1, exam_num);
         res = call.executeQuery();
         while(res.next()){
-//          System.out.println(res.getString(1));
+          System.out.println(res.getString(1));
+          System.out.println(res.getString(2));
           Student_Subject stu = new Student_Subject();
           stu.setSubject_name(res.getString(1));
           stu.setSubject_class(res.getString(2));
